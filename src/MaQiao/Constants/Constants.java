f765aefdb3df22eb1b2c0cf90291b55357d5a714
sunjian <sluizin@sohu.com>
2015-06-23 15:49:40 +0800

    2015-06-25

--- src/MaQiao/Constants/Constants.java
+++ src/MaQiao/Constants/Constants.java
@@ -70,44 +70,44 @@ public final class Constants {
 	 * @author SunJian
 	 */
 	public static enum FieldTypeEnum {
-		Object(new FieldType("java.lang.Object", "Ljava/lang/Object;", 0, null, "java.lang.Object", java.lang.Object.class, Unsafe.ARRAY_OBJECT_INDEX_SCALE)),
+		Object(new FieldType("java.lang.Object", "Ljava/lang/Object;", 0, null, "java.lang.Object", java.lang.Object.class, Unsafe.ARRAY_OBJECT_INDEX_SCALE, false)),
 
-		Char(new FieldType("java.lang.Character", "C", 1, null, "char", java.lang.Character.class, Unsafe.ARRAY_CHAR_INDEX_SCALE)),
+		Char(new FieldType("java.lang.Character", "C", 1, null, "char", java.lang.Character.class, Unsafe.ARRAY_CHAR_INDEX_SCALE, true)),
 
-		Int(new FieldType("java.lang.Integer", "I", 0, 0, "int", java.lang.Integer.class, Unsafe.ARRAY_INT_INDEX_SCALE)), Integer(new FieldType("java.lang.Integer", "Ljava/lang/Integer;", 0, 0,
-				"java.lang.Integer", java.lang.Integer.class, UNSAFE.arrayIndexScale(Integer[].class))),
+		Int(new FieldType("java.lang.Integer", "I", 0, 0, "int", java.lang.Integer.class, Unsafe.ARRAY_INT_INDEX_SCALE, true)), Integer(new FieldType("java.lang.Integer", "Ljava/lang/Integer;", 0, 0,
+				"java.lang.Integer", java.lang.Integer.class, UNSAFE.arrayIndexScale(Integer[].class), false)),
 
-		Boolean(new FieldType("java.lang.Boolean", "Z", 0, false, "boolean", java.lang.Boolean.class, Unsafe.ARRAY_BOOLEAN_INDEX_SCALE)), BooleanObject(new FieldType("java.lang.Boolean",
-				"Ljava/lang/Boolean;", 0, false, "java.lang.Boolean", java.lang.Boolean.class, UNSAFE.arrayIndexScale(Boolean[].class))),
+		Boolean(new FieldType("java.lang.Boolean", "Z", 0, false, "boolean", java.lang.Boolean.class, Unsafe.ARRAY_BOOLEAN_INDEX_SCALE, true)), BooleanObject(new FieldType("java.lang.Boolean",
+				"Ljava/lang/Boolean;", 0, false, "java.lang.Boolean", java.lang.Boolean.class, UNSAFE.arrayIndexScale(Boolean[].class), false)),
 
-		Float(new FieldType("java.lang.Float", "F", 0, null, "float", java.lang.Float.class, Unsafe.ARRAY_FLOAT_INDEX_SCALE)), FloatObject(new FieldType("java.lang.Float", "Ljava/lang/Float;", 0,
-				null, "java.lang.Float", java.lang.Float.class, UNSAFE.arrayIndexScale(Float[].class))),
+		Float(new FieldType("java.lang.Float", "F", 0, null, "float", java.lang.Float.class, Unsafe.ARRAY_FLOAT_INDEX_SCALE, true)), FloatObject(new FieldType("java.lang.Float", "Ljava/lang/Float;",
+				0, null, "java.lang.Float", java.lang.Float.class, UNSAFE.arrayIndexScale(Float[].class), false)),
 
-		Long(new FieldType("java.lang.Long", "J", 0, 0, "long", java.lang.Long.class, Unsafe.ARRAY_LONG_INDEX_SCALE)), LongObject(new FieldType("java.lang.Long", "Ljava/lang/Long;", 0, 0,
-				"java.lang.Long", java.lang.Long.class, UNSAFE.arrayIndexScale(Long[].class))),
+		Long(new FieldType("java.lang.Long", "J", 0, 0, "long", java.lang.Long.class, Unsafe.ARRAY_LONG_INDEX_SCALE, true)), LongObject(new FieldType("java.lang.Long", "Ljava/lang/Long;", 0, 0,
+				"java.lang.Long", java.lang.Long.class, UNSAFE.arrayIndexScale(Long[].class), false)),
 
-		Double(new FieldType("java.lang.Double", "D", 0, null, "double", java.lang.Double.class, Unsafe.ARRAY_DOUBLE_INDEX_SCALE)), DoubleObject(new FieldType("java.lang.Double",
-				"Ljava/lang/Double;", 0, null, "java.lang.Double", java.lang.Double.class, UNSAFE.arrayIndexScale(Double[].class))),
+		Double(new FieldType("java.lang.Double", "D", 0, null, "double", java.lang.Double.class, Unsafe.ARRAY_DOUBLE_INDEX_SCALE, true)), DoubleObject(new FieldType("java.lang.Double",
+				"Ljava/lang/Double;", 0, null, "java.lang.Double", java.lang.Double.class, UNSAFE.arrayIndexScale(Double[].class), false)),
 
-		Byte(new FieldType("java.lang.Byte", "B", 0, null, "byte", java.lang.Byte.class, Unsafe.ARRAY_BYTE_INDEX_SCALE)), ByteObject(new FieldType("java.lang.Byte", "Ljava/lang/Byte;", 0, null,
-				"java.lang.Byte", java.lang.Byte.class, UNSAFE.arrayIndexScale(Byte[].class))),
+		Byte(new FieldType("java.lang.Byte", "B", 0, null, "byte", java.lang.Byte.class, Unsafe.ARRAY_BYTE_INDEX_SCALE, true)), ByteObject(new FieldType("java.lang.Byte", "Ljava/lang/Byte;", 0, null,
+				"java.lang.Byte", java.lang.Byte.class, UNSAFE.arrayIndexScale(Byte[].class), false)),
 
-		Short(new FieldType("java.lang.Short", "S", 0, null, "short", java.lang.Short.class, Unsafe.ARRAY_SHORT_INDEX_SCALE)), ShortObject(new FieldType("java.lang.Short", "Ljava/lang/Short;", 0,
-				null, "java.lang.Short", java.lang.Short.class, UNSAFE.arrayIndexScale(Short[].class))),
+		Short(new FieldType("java.lang.Short", "S", 0, null, "short", java.lang.Short.class, Unsafe.ARRAY_SHORT_INDEX_SCALE, true)), ShortObject(new FieldType("java.lang.Short", "Ljava/lang/Short;",
+				0, null, "java.lang.Short", java.lang.Short.class, UNSAFE.arrayIndexScale(Short[].class), false)),
 
-		String(new FieldType("java.lang.String", "Ljava/lang/String;", 1, null, "java.lang.String", java.lang.String.class, UNSAFE.arrayIndexScale(String[].class))),
+		String(new FieldType("java.lang.String", "Ljava/lang/String;", 1, null, "java.lang.String", java.lang.String.class, UNSAFE.arrayIndexScale(String[].class), false)),
 
-		Timestamp(new FieldType("java.sql.Timestamp", "Ljava/sql/Timestamp;", 0, null, "java.sql.Timestamp", java.sql.Timestamp.class, UNSAFE.arrayIndexScale(java.sql.Timestamp[].class))),
+		Timestamp(new FieldType("java.sql.Timestamp", "Ljava/sql/Timestamp;", 0, null, "java.sql.Timestamp", java.sql.Timestamp.class, UNSAFE.arrayIndexScale(java.sql.Timestamp[].class), false)),
 
-		ListObject(new FieldType("java.util.List", "Ljava/util/List;", 2, null, "java.util.List", java.util.List.class, UNSAFE.arrayIndexScale(java.util.List[].class))),
+		ListObject(new FieldType("java.util.List", "Ljava/util/List;", 2, null, "java.util.List", java.util.List.class, UNSAFE.arrayIndexScale(java.util.List[].class), false)),
 
-		MapObject(new FieldType("java.util.Map", "Ljava/util/Map;", 2, null, "java.util.Map", java.util.Map.class, UNSAFE.arrayIndexScale(java.util.Map[].class))),
+		MapObject(new FieldType("java.util.Map", "Ljava/util/Map;", 2, null, "java.util.Map", java.util.Map.class, UNSAFE.arrayIndexScale(java.util.Map[].class), false)),
 
-		SetObject(new FieldType("java.util.Set", "Ljava/util/Set;", 2, null, "java.util.Set", java.util.Set.class, UNSAFE.arrayIndexScale(java.util.Set[].class))),
+		SetObject(new FieldType("java.util.Set", "Ljava/util/Set;", 2, null, "java.util.Set", java.util.Set.class, UNSAFE.arrayIndexScale(java.util.Set[].class), false)),
 
-		HashMapObject(new FieldType("java.util.HashMap", "Ljava/util/HashMap;", 2, null, "java.util.HashMap", java.util.HashMap.class, UNSAFE.arrayIndexScale(java.util.HashMap[].class))),
+		HashMapObject(new FieldType("java.util.HashMap", "Ljava/util/HashMap;", 2, null, "java.util.HashMap", java.util.HashMap.class, UNSAFE.arrayIndexScale(java.util.HashMap[].class), false)),
 
-		BitSetObject(new FieldType("java.util.BitSet", "Ljava/util/BitSet;", 2, null, "java.util.BitSet", java.util.BitSet.class, UNSAFE.arrayIndexScale(java.util.BitSet[].class)));
+		BitSetObject(new FieldType("java.util.BitSet", "Ljava/util/BitSet;", 2, null, "java.util.BitSet", java.util.BitSet.class, UNSAFE.arrayIndexScale(java.util.BitSet[].class), false));
 		/**
 		 * String point
 		 */
@@ -126,6 +126,15 @@ public final class Constants {
 
 		public final FieldType point() {
 			return point;
+		}
+
+		/**
+		 * 判断Field是否存在枚举对象
+		 * @param Field
+		 * @return FieldTypeEnum
+		 */
+		public static final FieldTypeEnum getByField(final Field field) {
+			return getByReflectFields(field.getType().getName());
 		}
 
 		/**
@@ -253,5 +262,70 @@ public final class Constants {
 		case Object:
 			break;
 		}*/
+	/**
+	 * 通过unsafe得到对象偏移位置的对象，类型已经通过FieldTypeEnum确认<br/>
+	 * 在获取identity时，注意：Short,Float,Long,Double对象，需要提取值，则不是地址<br/>
+	 * 暂时无法提取这几种类型的地址<br/>
+	 * @param obj Object
+	 * @param offSet long
+	 * @param fte FieldTypeEnum
+	 * @return double
+	 */
+	public static final double getUNSAFEObjectidentityHashCode(final Object obj, final long offSet, final FieldTypeEnum fte) {
+		if (fte == null) return 0;
+		switch (fte) {
+		case Char:
+			return System.identityHashCode(UNSAFE.getChar(obj, offSet));
+		case Int:
+			return System.identityHashCode(UNSAFE.getInt(obj, offSet));
+		case Boolean:
+			return System.identityHashCode(UNSAFE.getBoolean(obj, offSet));
+		case Float:
+			return UNSAFE.getFloat(obj, offSet);
+		case Long:
+			return UNSAFE.getLong(obj, offSet);
+		case Double:
+			return UNSAFE.getDouble(obj, offSet);
+		case Byte:
+			return System.identityHashCode(UNSAFE.getByte(obj, offSet));
+		case Short:
+			return UNSAFE.getShort(obj, offSet);
+		default:
+			return 0;
+		}
+	}
 
+	/**
+	 * 调查private/public/protected 非静态变量的IdentityHashCode
+	 * @param obj Object
+	 * @param field Field
+	 * @return double
+	 */
+	public static final double getIdentityHashCode(final Object obj, final Field field) {
+		if (field == null) return 0;
+		int identityHashCode = 0;
+		if (!((field.getModifiers() & java.lang.reflect.Modifier.STATIC) == java.lang.reflect.Modifier.STATIC)) {
+			FieldTypeEnum fte = null;
+			if ((fte = FieldTypeEnum.getByField(field)) != null && fte.point.isUNSAFEget()) return getUNSAFEObjectidentityHashCode(obj, UNSAFE.objectFieldOffset(field), fte);
+			try {
+				/*public变量，直接提出值*/
+				if ((field.getModifiers() & java.lang.reflect.Modifier.PUBLIC) == java.lang.reflect.Modifier.PUBLIC) return System.identityHashCode(field.get(obj));
+				/*private变量，直接setAccessible(true)提出值*/
+				if ((field.getModifiers() & java.lang.reflect.Modifier.PRIVATE) == java.lang.reflect.Modifier.PRIVATE) {
+					field.setAccessible(true);
+					return System.identityHashCode(field.get(obj));
+				}
+				/*protected变量，直接setAccessible(true)提出值*/
+				if ((field.getModifiers() & java.lang.reflect.Modifier.PROTECTED) == java.lang.reflect.Modifier.PROTECTED) {
+					field.setAccessible(true);
+					return System.identityHashCode(field.get(obj));
+				}
+			} catch (IllegalArgumentException e) {
+				e.printStackTrace();
+			} catch (IllegalAccessException e) {
+				e.printStackTrace();
+			}
+		}
+		return identityHashCode;
+	}
 }
--- src/MaQiao/Constants/FieldType.java
+++ src/MaQiao/Constants/FieldType.java
@@ -6,7 +6,7 @@ public final class FieldType {
 	//ASM Type
 	private String ASMType = "";
 	//Json中 Value值状态，默认0(直接连接),1:加",2:集合[] 
-	private int JsonType = 0;
+	private int jsonType = 0;
 	//ASM DefaultObject
 	private Object ASMDefaultObject = null;
 	// Fields Type
@@ -15,6 +15,8 @@ public final class FieldType {
 	private Class<?> FieldsClass = null;
 	//unsafe 类型元素 ARRAY_INDEX_SCALE偏移量
 	private int ARRAY_INDEX_SCALE = 0;
+	//可以通过unsafe的getXXXX提取
+	private boolean isUNSAFEget = false;
 
 	/**
 	 * 构造函数
@@ -22,6 +24,7 @@ public final class FieldType {
 	public FieldType() {
 
 	}
+
 	/**
 	 * 构造函数
 	 * @param JavaRsType java得到的recordSet数据类型
@@ -32,30 +35,32 @@ public final class FieldType {
 	 * @param FieldsClass Fields所需要 Class<?> 类
 	 * @param ARRAY_INDEX_SCALE unsafe 类型元素 数组偏移量
 	 */
-	public FieldType(final String JavaRsType, final String ASMType, final int JsonType, final Object ASMDefaultObject, final String ReflectFields, final Class<?> FieldsClass, int ARRAY_INDEX_SCALE) {
+	public FieldType(final String JavaRsType, final String ASMType, final int JsonType, final Object ASMDefaultObject, final String ReflectFields, final Class<?> FieldsClass,
+			final int ARRAY_INDEX_SCALE, final boolean isUNSAFEget) {
 		this.JavaRsType = JavaRsType;
 		this.ASMType = ASMType;
-		this.JsonType = JsonType;
+		this.jsonType = JsonType;
 		this.ASMDefaultObject = ASMDefaultObject;
 		this.ReflectFields = ReflectFields;
 		this.FieldsClass = FieldsClass;
 		this.ARRAY_INDEX_SCALE = ARRAY_INDEX_SCALE;
+		this.isUNSAFEget = isUNSAFEget;
 	}
 
 	public final String getJavaRsType() {
 		return JavaRsType;
 	}
 
-	public final void setJavaRsType(final String JavaRsType) {
-		this.JavaRsType = JavaRsType;
+	public final void setJavaRsType(final String javaRsType) {
+		this.JavaRsType = javaRsType;
 	}
 
 	public final String getASMType() {
 		return ASMType;
 	}
 
-	public final void setASMType(final String ASMType) {
-		this.ASMType = ASMType;
+	public final void setASMType(final String aSMType) {
+		this.ASMType = aSMType;
 	}
 
 	/**
@@ -63,35 +68,35 @@ public final class FieldType {
 	 * @return int
 	 */
 	public final int getJsonType() {
-		return JsonType;
+		return jsonType;
 	}
 
-	public final void setJsonType(final int JsonType) {
-		this.JsonType = JsonType;
+	public final void setJsonType(final int jsonType) {
+		this.jsonType = jsonType;
 	}
 
 	public final Object getASMDefaultObject() {
 		return ASMDefaultObject;
 	}
 
-	public final void setASMDefaultObject(final Object ASMDefaultObject) {
-		this.ASMDefaultObject = ASMDefaultObject;
+	public final void setASMDefaultObject(final Object aSMDefaultObject) {
+		this.ASMDefaultObject = aSMDefaultObject;
 	}
 
 	public final String getReflectFields() {
 		return ReflectFields;
 	}
 
-	public final void setReflectFields(final String ReflectFields) {
-		this.ReflectFields = ReflectFields;
+	public final void setReflectFields(final String reflectFields) {
+		this.ReflectFields = reflectFields;
 	}
 
 	public final Class<?> getFieldsClass() {
 		return FieldsClass;
 	}
 
-	public final void setFieldsClass(final Class<?> FieldsClass) {
-		this.FieldsClass = FieldsClass;
+	public final void setFieldsClass(final Class<?> fieldsClass) {
+		this.FieldsClass = fieldsClass;
 	}
 
 	public final int getARRAY_INDEX_SCALE() {
@@ -102,25 +107,36 @@ public final class FieldType {
 		this.ARRAY_INDEX_SCALE = ARRAY_INDEX_SCALE;
 	}
 
+	public final boolean isUNSAFEget() {
+		return isUNSAFEget;
+	}
+
+	public final void setUNSAFEget(boolean isUNSAFEget) {
+		this.isUNSAFEget = isUNSAFEget;
+	}
+
 	@Override
-	public final int hashCode() {
+	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
+		result = prime * result + ARRAY_INDEX_SCALE;
 		result = prime * result + ((ASMDefaultObject == null) ? 0 : ASMDefaultObject.hashCode());
 		result = prime * result + ((ASMType == null) ? 0 : ASMType.hashCode());
 		result = prime * result + ((FieldsClass == null) ? 0 : FieldsClass.hashCode());
 		result = prime * result + ((JavaRsType == null) ? 0 : JavaRsType.hashCode());
-		result = prime * result + JsonType;
 		result = prime * result + ((ReflectFields == null) ? 0 : ReflectFields.hashCode());
+		result = prime * result + (isUNSAFEget ? 1231 : 1237);
+		result = prime * result + jsonType;
 		return result;
 	}
 
 	@Override
-	public final boolean equals(Object obj) {
+	public boolean equals(Object obj) {
 		if (this == obj) return true;
 		if (obj == null) return false;
-		if (getClass() != obj.getClass()) return false;
+		if (!(obj instanceof FieldType)) return false;
 		FieldType other = (FieldType) obj;
+		if (ARRAY_INDEX_SCALE != other.ARRAY_INDEX_SCALE) return false;
 		if (ASMDefaultObject == null) {
 			if (other.ASMDefaultObject != null) return false;
 		} else if (!ASMDefaultObject.equals(other.ASMDefaultObject)) return false;
@@ -133,17 +149,18 @@ public final class FieldType {
 		if (JavaRsType == null) {
 			if (other.JavaRsType != null) return false;
 		} else if (!JavaRsType.equals(other.JavaRsType)) return false;
-		if (JsonType != other.JsonType) return false;
 		if (ReflectFields == null) {
 			if (other.ReflectFields != null) return false;
 		} else if (!ReflectFields.equals(other.ReflectFields)) return false;
+		if (isUNSAFEget != other.isUNSAFEget) return false;
+		if (jsonType != other.jsonType) return false;
 		return true;
 	}
 
 	@Override
-	public final String toString() {
-		return "FieldType:[JavaRsType=" + JavaRsType + ", ASMType=" + ASMType + ", JsonType=" + JsonType + ", ASMDefaultObject=" + ASMDefaultObject + ", ReflectFields=" + ReflectFields
-				+ ", FieldsClass=" + FieldsClass + "]";
+	public String toString() {
+		return "FieldType [JavaRsType=" + JavaRsType + ", ASMType=" + ASMType + ", jsonType=" + jsonType + ", ASMDefaultObject=" + ASMDefaultObject + ", ReflectFields=" + ReflectFields
+				+ ", FieldsClass=" + FieldsClass + ", ARRAY_INDEX_SCALE=" + ARRAY_INDEX_SCALE + ", isUNSAFEget=" + isUNSAFEget + "]";
 	}
 
 }
